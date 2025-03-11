1a20d1bca88b35ce68163d4a0c12982ea09d8e7c
mr-tajul-Islam <tajul.azazil@gmail.com>
2025-03-11 15:11:46 +0600

    assets and index file added

---
+++ assets/scripts/scripts.js
@@ -0,0 +1,39 @@
+{
+           "@context": "http://www.schema.org",
+           "@type": "ProfessionalService",
+           "name": "MarketingAI",
+           "title": "MarketingAI - Comprehensive Learning for Career Success",
+           "url": "/",
+           "sameAs": [
+              ""
+           ],
+           "logo": "https://moksy.com/api/build/uploads/37085771/uploads/logo.png",
+           "priceRange": "$$$",
+           "image": "https://moksy.com/api/build/uploads/37085771/uploads/socialshare.png",
+           "description": "Achieve career success with comprehensive online learning from MarketingAI. Explore our courses and start your journey towards success.",
+           "address": {
+              "@type": "PostalAddress",
+              "streetAddress": "3-5, Marco Polo House, Lansdowne Road",
+              "addressLocality": "London",
+              "addressRegion": "Surrey",
+              "postalCode": "CR0 2BX",
+              "addressCountry": "United Kingdom"
+           },
+           "email": "support@moksy.com",
+           "geo": {
+              "@type": "GeoCoordinates",
+              "latitude": "51.37683",
+              "longitude": "-0.09728"
+           },
+           
+           "openingHours": "Mo 09:00-17:00 Tu 09:00-17:00 We 09:00-17:00 Th 09:00-17:00 Fr 09:00-17:00",
+           "telephone": "+44 0808 175 1749",
+           "aggregateRating": {
+                 "@type": "AggregateRating",
+                 "ratingValue": "5",
+                 "ratingCount": "100"
+               },
+          "datePublished": "2022-08-23 17:08:07"
+         }
+document.write(new Date().getFullYear());
+Value for custom_js_code
